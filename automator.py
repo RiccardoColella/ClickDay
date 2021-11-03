87b10de47e683f4e7851455737965bf659589c02
RiccardoColella <colella.git@outlook.com>
2021-11-03 22:26:35 +0100

    Updated form procedure

--- automator.py
+++ automator.py
@@ -1,8 +1,11 @@
-from datetime import date
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 import re
 import json
+import time
+import datetime
 
 f = open("secret.json")
 secret_data = json.load(f)
@@ -10,49 +13,51 @@ secret_data = json.load(f)
 driver = webdriver.Chrome()
 driver.get(secret_data['login_page'])
 # Login
-driver.find_element_by_id('insert_login').send_keys(secret_data['user'])
-driver.find_element_by_id('insert_pwd').send_keys(secret_data['psw'])
-driver.find_element_by_xpath('/html/body/div/div/form/div[3]/input').click()
+driver.find_element(By.ID, 'insert_login').send_keys(secret_data['user'])
+driver.find_element(By.ID, 'insert_pwd').send_keys(secret_data['psw'])
+driver.find_element(By.XPATH, '/html/body/div/div/form/div[3]/input').click()
 driver.get(secret_data['simulator_page'])
 
 
 def routine(iteration=-1):
     # Till the button "reload" is present: reload
-    while driver.find_element_by_xpath('//*[@id="invia-btn"]').is_displayed():
-        driver.find_element_by_xpath('//*[@id="invia-btn"]').click()
-
-    today = date.today()
+    while driver.find_element(By.XPATH, '//*[@id="invia-btn"]').is_displayed():
+        driver.find_element(By.XPATH, '//*[@id="invia-btn"]').click()
 
     # First drop-down menu
-    select = Select(driver.find_element_by_xpath('//*[@id="tipo-bando"]'))
+    select = Select(driver.find_element(By.XPATH, '//*[@id="tipo-bando"]'))
     select.select_by_value('ISI2020')
-    # ID code
-    text = driver.find_element_by_xpath('//*[@id="input-token-fe"]/label').text
-    code = driver.find_element_by_xpath('//*[@id="my-token-value"]/strong').text
 
-    n = 11
-    if re.search("due", text):
-        n = 2
-    elif re.search("tre", text):
-        n = 3
-    elif re.search("quattro", text):
-        n = 4
-    elif re.search("cinque", text):
-        n = 5
-    elif re.search("sei", text):
-        n = 6
-    elif re.search("sette", text):
-        n = 7
-    elif re.search("otto", text):
-        n = 8
-    elif re.search("nove", text):
-        n = 9
-    elif re.search("dieci", text):
-        n = 10
-    if re.search("^\* Inserire gli ultimi", text):
-        # END REGEX
+    # ID code
+    text = driver.find_element(By.XPATH, '//*[@id="input-token-fe"]/label').text
+    code = driver.find_element(By.XPATH, '//*[@id="my-token-fe"]/input').get_property("value")
+
+    n = int(driver.find_element(By.XPATH, '//*[@id="input-token"]').get_attribute("maxlength"))
+    groups = 1
+
+    if re.search("numer", text):
+        base_re = "[a-z]*([0-9]*)[a-z]*"
+    else:
+        base_re = "[0-9]*([a-z]*)[0-9]*"
+
+    last = False
+
+    if re.search("^\* Inserire gli ultimi [a-z]+ caratteri", text):
         starting = 64 - n
         regex = "^\+[\w]{" + str(starting) + "}([\w]{" + str(n) + "})"
+    # Ultime cifre oppure ultime lettere
+    elif re.search("^\* Inserire .+ ultim", text):
+        regex = "^\+[0-9a-z]{30}" + base_re * 30
+        groups = 30
+        last = True
+    # Prime cifre oppure prime lettere
+    elif re.search("^\* Inserire .+ prim", text):
+        regex = "^\+" + base_re * n
+        groups = n
+    # Numero tra parentesi
+    elif re.search("^\* Inserire il numero tra parentesi", text):
+        regex = "tra parentesi \(([0-9]+)\)"
+    # Prime cifre o lettere
     else:
         # START REGEX
         regex = "^\+([\w]{" + str(n) + "})"
@@ -62,34 +67,68 @@ def routine(iteration=-1):
         trial = re.search(regex, code)
         print("WTF!")
     try:
-        requested_code = re.match(regex, code).group(1)
+        requested_code = ""
+        for i in range(groups):
+            requested_code += re.match(regex, code).group(i+1)
     except AttributeError:
         print("Oh noo...")
         print(text)
-        print(code)
+        print("||" + code + "||")
         print(regex)
-        requested_code = re.match(regex, code)
+        print("Is match?")
+        print(re.match(regex, code).group())
+        print("has it been match?")
+        requested_code = "REINITIALISED-"
+        for i in range(groups):
+            requested_code += re.match(regex, code).group(i+1)
         print(requested_code)
-    driver.find_element_by_xpath('//*[@id="input-token"]').send_keys(requested_code)
+    if last:
+        requested_code = requested_code[-n:]
+    driver.find_element(By.XPATH, '//*[@id="input-token"]').send_keys(requested_code[0:n])
 
     # Date
-    driver.find_element_by_xpath('//*[@id="data-odierna"]').send_keys(today.strftime("%d/%m/%Y"))
+    text = driver.find_element(By.XPATH, '//*[@id="data-odierna-fe"]/label').text
+    data = datetime.date.today()
+    if re.search("^\* Data di domani", text):
+        data += datetime.timedelta(days=1)
+    elif re.search("^\* Data di ieri", text):
+        data -= datetime.timedelta(days=1)
+    data = data.strftime("%d/%m/%Y")
+    driver.find_element(By.XPATH, '//*[@id="data-odierna"]').send_keys(data)
+
+    # Operation
+    try:
+        operation_result = driver.find_element(By.XPATH, '//*[@id="trueResult"]').get_attribute("value")
+        driver.find_element(By.XPATH, '//*[@id="operazione"]').send_keys(operation_result)
+    except NoSuchElementException:
+        print("No operation needed")
+
     # Second drop-down menu
-    driver.find_element_by_xpath('//*[@id="data-momento3"]/option[4]').click()
+    driver.find_element(By.XPATH, '//*[@id="data-momento3"]/option[4]').click()
+
     # tick
-    driver.find_element_by_xpath('//*[@id="presa-visione-check"]').click()
-    driver.find_element_by_xpath('//*[@id="no-robot-cb"]').click()
+    driver.find_element(By.XPATH, '//*[@id="presa-visione-check"]').click()
+    driver.find_element(By.XPATH, '//*[@id="no-robot-cb"]').click()
+
+    # Set bot-detector to false before sending the form
+    driver.execute_script('document.getElementById("botDetector").value="not-detected"')
+
     # continue
-    driver.find_element_by_xpath('//*[@id="avanti-btn"]').click()
+    driver.find_element(By.XPATH, '//*[@id="avanti-btn"]').click()
+
     # ticks
-    driver.find_element_by_xpath('//*[@id="consenso-verifica-cb-fe"]/div/label').click()
-    driver.find_element_by_xpath('//*[@id="invia-btn"]').click()
+    driver.find_element(By.XPATH, '//*[@id="consenso-verifica-cb-fe"]/div/label').click()
+    # Set bot-detector to false before sending the form
+    driver.execute_script('document.getElementById("botDetector").value="not-detected"')
+
+    driver.find_element(By.XPATH, '//*[@id="invia-btn"]').click()
 
     # result
-    result = driver.find_element_by_xpath('/html/body/div/div[2]/p[2]/strong').text
+    result = driver.find_element(By.XPATH, '/html/body/div/div[2]/p[2]/strong').text
     print("Iteration " + str(iteration) + " - Time employed: " + str(result))
+    time.sleep(4)
     # New simulation
-    driver.find_element_by_xpath('//*[@id="invia-btn"]').click()
+    driver.find_element(By.XPATH, '//*[@id="invia-btn"]').click()
 
 
 iteration_n = 0
